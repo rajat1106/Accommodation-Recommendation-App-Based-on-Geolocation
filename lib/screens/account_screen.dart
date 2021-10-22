0abb449093819f3e3315287e677e5b72b2c83403
@rajat1106 <rajatharne@gmail.com>
2021-10-22 22:33:35 +0530

    KK

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sdp_v2/pallaete.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'login.dart';
